f0595c19e201c0fbc9762c87d4cf023be36fd983
Devdeep Saha <139770607+devdeepsaha@users.noreply.github.com>
2025-09-04 12:55:09 +0530

    Update Chatbot.jsx

--- bccl-frontend/src/components/Chatbot/Chatbot.jsx
+++ bccl-frontend/src/components/Chatbot/Chatbot.jsx
@@ -16,7 +16,7 @@ const Chatbot = () => {
 
         setTimeout(async () => {
             try {
-                const response = await fetch('http://127.0.0.1:5000/api/chat', {
+                const response = await fetch('https://bccl-backend.onrender.com', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ message }),
